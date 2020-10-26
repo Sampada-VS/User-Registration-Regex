19aa970166ed2d6e12e3b9efe255a6a77c6a0fd2
Sampada-VS <samssvv1@gmail.com>
2020-10-27 00:43:21 +0530

    Adding -to check if password has min 8 characters

--- src/com/registrationregex/UserRegistration.java
+++ src/com/registrationregex/UserRegistration.java
@@ -1,7 +1,6 @@
 package com.registrationregex;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
@@ -10,11 +9,11 @@ public class UserRegistration {
 
 		System.out.println("== User Registration Form ==");
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter Mobile Number :");
 		String mobNo = sc.nextLine();
 		validateMobileNumber(mobNo);
-		
+
 		System.out.println("Enter First Name :");
 		String firstName = sc.next();
 		validateName(firstName);
@@ -22,24 +21,27 @@ public class UserRegistration {
 		System.out.println("Enter Last Name :");
 		String lastName = sc.next();
 		validateName(lastName);
-		
+
 		System.out.println("Enter Email id :");
 		String email = sc.next();
 		validateEmail(email);
-		
 
-
+		System.out.println("Enter Password :");
+		String password = sc.next();
+		validatePassword(password);
 		sc.close();
-		
+
 	}
+
 	static void validateMobileNumber(String number) {
 		String mobNoRegex = "^[1-9]{2,3}\\s[7-9]{1}[0-9]{9}$";
-		
+
 		if (Pattern.compile(mobNoRegex).matcher(number).matches())
 			System.out.println("Valid");
 		else
-		    System.out.println("Mobile number should contain country code follow by space and 10 digit number.");
+			System.out.println("Mobile number should contain country code follow by space and 10 digit number.");
 	}
+
 	static void validateName(String name) {
 		String nameRegex = "^[A-Z][a-z]{2,}$";
 		if (Pattern.compile(nameRegex).matcher(name).matches())
@@ -57,5 +59,13 @@ public class UserRegistration {
 			System.out.println("Invalid email id");
 	}
 	
-	
+	static void validatePassword(String password) {
+		String passwordRegex = "[\\w\\W]{8,}";
+
+		if (Pattern.compile(passwordRegex).matcher(password).matches())
+			System.out.println("Valid");
+		else
+			System.out.println("Password should contain minimum 8 characters.");
+	}
+
 }
